0b197407e986124af510f7b0ef2e8cbe7fce751f
Andre Xavier <lordlagon@gmail.com>
2019-10-12 11:06:47 -0300

    Add project files.

---
+++ CooperativaXZ_Front.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace CooperativaXZ_Front.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
